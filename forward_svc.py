2883836c1770045a030bb146e00a5b61a18da1be
dmitry nagovitsin <dna@dmitrys-Mac-mini.local>
2017-04-07 18:50:27 +0300

    fix some

--- forward_svc.py
+++ forward_svc.py
@@ -12,9 +12,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 
-def parse_test():
+
+def wait_message():
         if not request.json or not 'text' in request.json or not 'chatid' in request.json or not 'token' in request.json:
                 abort(400)
+        
         msg = {
                'token': request.json['token'], 
                'chatid': request.json['chatid'], 
@@ -22,14 +24,13 @@ def parse_test():
         }
 
         msg_chat = urllib.quote(request.json['text'])
+
         msg_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(request.json['token'], request.json['chatid'], msg_chat ).encode('UTF-8')
-        print(msg_url)  #debug stuff
 
         get = urllib2.urlopen(msg_url)
 
         return jsonify({'text': msg}), 201
 
+
 if __name__ == "__main__":
         app.run(host=listen_ip, port=listen_port)
-
-
